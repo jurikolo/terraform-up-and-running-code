1cfa48f8b89d94becd11fb58f961d5c06da14728
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-03-09 23:12:17 +0200

    Upgrade to terraform 0.13

--- code/terraform/01-why-terraform/web-server/main.tf
+++ code/terraform/01-why-terraform/web-server/main.tf
@@ -1,18 +1,21 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      version = "~> 2.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  region = "eu-central-1"
+  profile = "terraform"
 }
 
 resource "aws_instance" "app" {
-  instance_type     = "t2.micro"
-  availability_zone = "us-east-2a"
-  ami               = "ami-0c55b159cbfafe1f0"
+  instance_type     = "t4g.micro"
+  availability_zone = "eu-central-1a"
+  ami               = "ami-016f4f002606a1417"
 
   user_data = <<-EOF
               #!/bin/bash
