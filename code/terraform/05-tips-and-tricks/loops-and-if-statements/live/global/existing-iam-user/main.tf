2ad874d738fa360dc108b14101dcd9879ff998f6
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-03-15 16:08:51 +0200

    Upgrade to terraform 0.13

--- code/terraform/05-tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
+++ code/terraform/05-tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
@@ -1,17 +1,20 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      version = "~> 2.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  region = "eu-central-1"
+  profile = "terraform"
 }
 
 resource "aws_iam_user" "existing_user" {
   # You should change this to the username of an IAM user that already
   # exists so you can practice using the terraform import command
-  name = "yevgeniy.brikman"
+  name = "jurijs.kolomijecs"
 }
 
